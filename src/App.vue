2057540f0cd7bec67130f38f35cdfe3b60d8218e
Jean Tugangui <tugangui@g.ucla.edu>
2021-09-15 12:53:52 -0700

    Prototype

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,106 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
-</template>
-
 <script>
+import Carousel from './components/Carousel'
+import imageData from './assets/data.json'
+
 export default {
-  name: 'app',
+  name : 'app',
+  components: {
+    Carousel
+  },
+  methods: {
+    selectOnlyThis(channelNumber) {
+      this.selectedChannels = [];
+      for (var i = 1;i <= 4; i++) {
+        document.getElementById("ch" + i).checked = false;
+      }
+      document.getElementById("ch" + channelNumber).checked = true;
+      this.selectedChannels.push(channelNumber);
+    }
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+        images: imageData,
+        selectedChannels: [0]
     }
   }
 }
 </script>
 
-<style>
+<template>
+  <div id="app" class="row">
+    <div class="col-2 toggle-menu">
+      <p id="sidebarTitle">Channel</p>
+      <div id="toggles">
+      <div id="atlas" class="channel">
+        <input type="checkbox" id="ch0" value="0" v-model="selectedChannels" v-on:click="selectOnlyThis(0)">
+        <label for="atlas">None (Atlas)</label>
+      </div>
+
+      <div id="neun" class="channel">
+        <input type="checkbox" id="ch1" value="1" v-model="selectedChannels" v-on:click="selectOnlyThis(1)">
+        <label for="neun">NeuN</label>
+      </div>
+      <div id="th" class="channel">
+        <input type="checkbox" id="ch2" value="2" v-model="selectedChannels" v-on:click="selectOnlyThis(2)">
+        <label for="th">Tyrosine Hydroxylase</label>
+      </div>
+      <div id="nissl" class="channel">
+        <input type="checkbox" id="ch3" value="3" v-model="selectedChannels" v-on:click="selectOnlyThis(3)">
+        <label for="nissl">NISSL</label>
+      </div>
+      <div id="ca" class="channel">
+        <input type="checkbox" id="ch4" value="4" v-model="selectedChannels" v-on:click="selectOnlyThis(4)">
+        <label for="ca">Choline Acetyltransferase</label>
+      </div>
+      </div>
+    </div>
+    <carousel :slides="images" :selectedChannels="selectedChannels"/>
+    </div>
+</template>
+
+<style scoped>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: black;
+  color: white;
+  font-family: "Helvetica Neue";
+  margin-left:0px;
+  margin-right:0px;
 }
 
-h1, h2 {
-  font-weight: normal;
+#sidebarTitle {
+  font-weight: bold;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
+#toggles {
+  border: 1px grey solid;
+  border-radius: 3px;
+  background-color: #141414;
 }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
+.channel {
+  font-weight: 700;
+  font-size: 14px;
+  padding: 5px;
 }
 
-a {
-  color: #42b983;
+.toggle-menu {
+  top: 50px;
+}
+
+#neun {
+  color: purple;
+}
+
+#th {
+  color: green;
+}
+
+#nissl {
+  color: blue;
+}
+
+#ca {
+  color: red;
 }
 </style>
+ 
---
+++ src/components/Carousel.vue
@@ -0,0 +1,72 @@
+<script>
+  import { Carousel3d, Slide } from 'vue-carousel-3d'
+
+  export default {
+    components: {
+      Carousel3d,
+      Slide
+    },
+    props: {
+        slides: {
+          type: Array,
+          default: function () { return [] }
+        },
+        selectedChannels: Array
+    },
+    mounted: function() {
+      var self = this;
+      window.addEventListener('keyup', function(event) {
+        if (event.keyCode == 39) {
+            self.$refs.carousel.goNext();
+        }
+        if (event.keyCode == 37) { 
+            self.$refs.carousel.goPrev();
+        }
+      });
+    }
+  };
+
+</script>
+
+<template>
+    <div id="carousel" class="col-10">
+        <carousel-3d ref="carousel" :count="slides.length" :border="0" :perspective="0" :controls-visible="true" :controls-prev-html="'&#10092;'" :controls-next-html="'&#10093;'" :controls-width="30" :controls-height="660" :width="1200" :height="1000" :display="1" :inverseScaling="0" :startIndex="slides.length/2">
+            <slide v-for="(slide, i) in slides" :index="i">
+                <img class="carousel-image atlas" v-bind:src="slide.atlasHref"/>
+                <img v-bind:class="{ activeLayer: selectedChannels.includes(1) }" class="carousel-image" v-bind:src="slide.purpleHref"/>
+                <img v-bind:class="{ activeLayer: selectedChannels.includes(2) }" class="carousel-image" v-bind:src="slide.greenHref"/>
+                <img v-bind:class="{ activeLayer: selectedChannels.includes(3) }" class="carousel-image" v-bind:src="slide.blueHref"/>
+                <img v-bind:class="{ activeLayer: selectedChannels.includes(4) }" class="carousel-image" v-bind:src="slide.redHref"/>
+                <figcaption>Section {{slide.section}}</figcaption>
+            </slide>
+        </carousel-3d>
+    </div>
+</template>
+
+<style scoped>
+.carousel-image {
+  visibility: hidden;
+}
+
+.activeLayer, .atlas {
+  visibility: visible;
+}
+
+.carousel-3d-slide {
+  background-color: transparent;
+}
+
+.carousel-image {
+  position: absolute;
+  opacity: 0.8;
+}
+
+.atlas {
+  opacity: 1;
+}
+
+figcaption {
+  font-size: 20px;
+  text-align: center;
+}
+</style>
